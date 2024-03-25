9c1ac3413c75097c8d74491d389b61f3079f2ebf
colorfulshadow <colorfulshadowty@gmail.com>
2024-03-25 16:07:28 +0800

    init script

---
+++ setup_vps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Step 1: Setup SSH Key for Root User
+echo "Starting SSH key setup..."
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0j4PTgKgnLmK/IV/oXkpWJomlkE9X7x9M6O4JExkTWJsZNG0SBma2bpW39ronv6jx+TIVkeWpIEjNkwwPRcnDrmekmtu1Hsi9jFgK7R8WmpeX2U6l6ieBnomyH8HBSNNWRCHF62EzaJ1LLN6FpuJ4x6h3S2bAqePLYvtwvV2/v+XeR8Samh3lLMvW0b3oq8PpvNMUHZkwlPcGUxt9enJaNLFEP/8+g7pAbN2bn4T5ax0+au75svpoavzeZtS3QS0oNYJ1DZQ6eNzrQszB9uhNJfb+oupuQoGhMR61dEV0fF+gpjTuukmB8XLM9IsmhkVflyg4w5L/ArCHR+5hXjrXL+qjN8wXL22zb+IwYQQT684JXzpOBx+GFV9iCR02MAVIrb6oEHT4eZWuXM9fID+KyJd9aOlBxpEvHQAL/HIX3+z2Pa9TsjR3BKEGwrgmv6wIe44npi36M7BmyOjBTAgiH9TDEQeT68A3rjcgtJiPv1CjLnVu5mgxAgUZ0EUvjIc= zhang tianyi@Color" > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+echo "SSH key setup complete."
+
+# Step 2: Configure SSH to disable password authentication and enable key-based authentication
+echo "Configuring SSH..."
+sed -i 's/#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/#\?PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+echo "Restarting SSHD..."
+systemctl restart sshd
+echo "SSH configuration complete."
+
+# Step 3: Create /root/ssl folder and setup a cron job for SSL certificate update
+echo "Setting up SSL certificate update..."
+mkdir -p /root/ssl
+(crontab -l 2>/dev/null; echo "0 0 * * 0 cd /root/ssl && rm -rf cert* && wget ssl.colorduck.me/cert.tar.gz && tar -zxf cert.tar.gz") | crontab -
+cd /root/ssl && rm -rf cert* && wget ssl.colorduck.me/cert.tar.gz && tar -zxf cert.tar.gz
+echo "SSL certificate setup complete."
+
+echo "All tasks completed successfully."
---
+++ shpack.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# Define paths and URLs
+SHPACK_DIR="/usr/local/shpack/"
+SHPACK_URL="https://colorduck.me/shpack.tar.gz"
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+    fi
+fi
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum install wget curl tar -y
+    else
+        apt install wget curl tar -y
+    fi
+}
+
+install_shpack() {
+    echo "Installing shpack..."
+    cd /usr/local/
+    systemctl stop shpack
+    if [[ -e "$SHPACK_DIR" ]]; then
+        rm "$SHPACK_DIR" -rf
+    fi
+    mkdir -p "$SHPACK_DIR"
+    wget -O shpack.tar.gz "$SHPACK_URL"
+    tar -zxf shpack.tar.gz
+    rm shpack.tar.gz -f
+    cd shpack
+    chmod +x /usr/local/shpack/*
+    cp -f shpack.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable shpack
+    systemctl start shpack
+}
+
+update_shpack() {
+    echo "Updating shpack..."
+    rm -rf $SHPACK_DIR/*
+    wget -O "$SHPACK_DIR/shpack.tar.gz" "$SHPACK_URL"
+    tar -zxf "$SHPACK_DIR/shpack.tar.gz" -C "$SHPACK_DIR"
+}
+
+run_setup_ss() {
+    echo "Running setup_ss.sh..."
+    SETUP_SS_SCRIPT="$SHPACK_DIR/setup_ss.sh"
+    if [ -f "$SETUP_SS_SCRIPT" ]; then
+        bash "$SETUP_SS_SCRIPT"
+    else
+        echo "setup_ss.sh script does not exist in $SHPACK_DIR."
+    fi
+}
+
+run_setup_vps() {
+    echo "Running setup_vps.sh..."
+    SETUP_SS_SCRIPT="$SHPACK_DIR/setup_vps.sh"
+    if [ -f "$SETUP_SS_SCRIPT" ]; then
+        bash "$SETUP_SS_SCRIPT"
+    else
+        echo "setup_vps.sh script does not exist in $SHPACK_DIR."
+    fi
+}
+
+# Show menu
+show_menu() {
+    while true; do
+        echo -e "
+  shpack管理脚本
+  0. 退出脚本
+————————————————
+  1. 安装shpack
+  2. 更新shpack
+————————————————
+  3. 运行setup_ss.sh
+  4. 运行setup_vps.sh
+
+        read -p "请选择一个操作[0-6]: " option
+        case "$option" in
+            1) install_shpack ;;
+            2) update_shpack ;;
+            3) run_setup_ss ;;
+            4) run_setup_vps ;;
+            0) break ;;
+            *) echo "无效选项: $option" ;;
+        esac
+    done
+}
+
+main() {
+    install_base
+    show_menu
+}
+
+main "$@"
